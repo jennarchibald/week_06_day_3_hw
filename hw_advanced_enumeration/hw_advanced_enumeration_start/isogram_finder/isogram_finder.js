d529d0a58f9ff8677bd587fcf2f16333352af5db
jennarchibald <codeclan@codeclans-mbp.home>
2019-04-03 18:32:15 +0100

    refactoring

--- hw_advanced_enumeration/hw_advanced_enumeration_start/isogram_finder/isogram_finder.js
+++ hw_advanced_enumeration/hw_advanced_enumeration_start/isogram_finder/isogram_finder.js
@@ -5,18 +5,25 @@ const IsogramFinder = function (word) {
 
 IsogramFinder.prototype.isIsogram = function () {
 
-  const count = {};
-  this.chars.forEach( (char) => {
-    if (count[char]){
-      count[char] += 1
-    } else {
-      count[char] = 1
-    };
-  });
+  const count = this.countChars()
 
   return this.chars.every( (char) => {
     return count[char] <= 1;
   });
+};
+
+IsogramFinder.prototype.countChars = function () {
+
+  const count = {};
+    this.chars.forEach( (char) => {
+      if (count[char]){
+        count[char] += 1
+      } else {
+        count[char] = 1
+      };
+    });
+
+  return count;
 };
 
 module.exports = IsogramFinder;
