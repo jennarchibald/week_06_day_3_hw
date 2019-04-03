60a71a4c33fb1c51e99cda87472080c787534b9a
jennarchibald <codeclan@CodeClans-MacBook-Pro.local>
2019-04-03 15:32:20 +0100

    pangram finder

---
+++ hw_advanced_enumeration/hw_advanced_enumeration_start/pangram_finder/pangram_finder.js
@@ -0,0 +1,13 @@
+const PangramFinder = function (phrase) {
+  this.alphabet = 'qwertyuiopasdfghjklzxcvbnm'.split('');
+  this.phrase = phrase.toLowerCase().split('');
+}
+
+PangramFinder.prototype.isPangram = function () {
+
+  return this.alphabet.every( (letter) => {
+    return this.phrase.includes(letter);
+  });
+}
+
+module.exports = PangramFinder;
